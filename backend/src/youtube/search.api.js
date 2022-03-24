05a8049a67d07ed9fe52f61437cb4f87362bceaa
Nicola <lanzoni.nicola@gmail.com>
2022-03-23 22:27:19 -0300

    Fix pageToken in params

--- backend/src/youtube/search.api.js
+++ backend/src/youtube/search.api.js
@@ -18,24 +18,17 @@ export class YouTubeSearchAPI {
    *
    * @returns response.data - Data returned from the YouTube API not serialized
    */
-  videos(action, params) {
+  videos(params) {
     return new Promise((resolve, reject) => {
       const { channelId, pageToken } = params;
 
-      const cachedValue = this.cache.get(action, channelId, pageToken);
-
-      if (cachedValue) {
-        resolve(cachedValue);
-      } else {
-        client.search.list(params, (err, response) => {
-          if (err) {
-            reject(new YouTubeAPIError(err));
-          } else {
-            this.cache.set(action, channelId, pageToken, response.data);
-            resolve(response.data);
-          }
-        });
-      }
+      client.search.list(params, (err, response) => {
+        if (err) {
+          reject(new YouTubeAPIError(err));
+        } else {
+          resolve(response.data);
+        }
+      });
     });
   }
 }
@@ -49,11 +42,11 @@ export class SearchParams {
   /**
    * @description Constructs the params for the YouTube API to search for videos
    *
-   * @param channelId - The id of the channel to search for
-   * @param pageToken - The *pageToken* parameter identifies a specific page in the result set that should be returned. In an API response, the nextPageToken and prevPageToken properties identify other pages that could be retrieved,
-   * @param daysToDate - number - Filter on resources published X days ago - Default 120 days
+   * @param {string} channelId - The id of the channel to search for
+   * @param {string} pageToken - The *pageToken* parameter identifies a specific page in the result set that should be returned. In an API response, the nextPageToken and prevPageToken properties identify other pages that could be retrieved,
+   * @param {string} daysToDate - number - Filter on resources published X days ago - Default 120 days
    *
-   * @returns params - The params for the YouTube API to search for videos
+   * @returns {object} - params - The params for the YouTube API to search for videos
    */
   videos({ channelId, pageToken, daysToDate }) {
     const defaultDaysToday = 120;
@@ -61,11 +54,32 @@ export class SearchParams {
       ? pastDate(daysToDate)
       : pastDate(defaultDaysToday);
 
+    const params = {
+      ...this.baseParams,
+      channelId: channelId,
+      publishedAfter: _publishedAfter,
+      type: "video",
+    };
+
+    if (pageToken) {
+      params.pageToken = pageToken;
+    }
+
+    return params;
+  }
+
+  /**
+   * * @description Constructs the params for the YouTube API to search a specific video in a channel
+   *
+   * @param {string} channelId - The id of the channel to search for
+   * @param {string} videoId - The id of video
+   * @returns {object} params - The params for the YouTube API to search for a videoId
+   */
+  video({ channelId, videoId, pageToken }) {
     return {
       ...this.baseParams,
       channelId: channelId,
-      pageToken: pageToken ? pageToken : "",
-      publishedAfter: _publishedAfter,
+      q: videoId,
       type: "video",
     };
   }
